c6734fd66a48d53329a624d2959e8486a7196c23
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-08-14 12:57:42 +0200

    Updates

---
+++ precache-manifest.9f469c2ce6cf73e3f91b89e94d73a67b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d8a13dea23ed42eb004bdec753d9cd",
+    "url": "/react-covid19/index.html"
+  },
+  {
+    "revision": "31076d2f6b104a11fe99",
+    "url": "/react-covid19/static/css/main.375dc992.chunk.css"
+  },
+  {
+    "revision": "5f5b48c1db1002352747",
+    "url": "/react-covid19/static/js/2.d8b33a06.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid19/static/js/2.d8b33a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31076d2f6b104a11fe99",
+    "url": "/react-covid19/static/js/main.bed3ce9d.chunk.js"
+  },
+  {
+    "revision": "50aefc7dbb08c19dfa72",
+    "url": "/react-covid19/static/js/runtime-main.192c21f8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid19/static/media/covid-19.d7265326.png"
+  }
+]);
